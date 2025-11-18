e03ada68b4daeaf465e358833df63b50bbf6a674
Aditya Ganesh Shanbhag <aditya.shanbhag.dev@gmail.com>
2025-11-18 19:07:07 +0530

    Initial setup

---
+++ components/header.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Image from "next/image";
+import { IconBrandGithub } from '@tabler/icons-react';
+
+export default function Header() {
+  return (
+    <div className="w-full mt-0 h-auto p-2 z-50">
+      <div className="flex items-center justify-between mx-5">
+        <div className="flex items-center cursor-pointer" onClick={() => window.location.href = "/"}>
+          <Image src="/vibreon-logo.png" alt="Vibreon Logo" width={80} height={80} />
+          <Image src="/vibreon-title.png" alt="Vibreon Title" width={125} height={80} />
+        </div>
+        <div className="flex items-center gap-4 mr-5">
+          <IconBrandGithub className="w-6 h-6 cursor-pointer" onClick={() => window.open("https://github.com/aditya-shanbhag-dev/vibreon")} />
+        </div>
+      </div>
+    </div>
+  );
+}
