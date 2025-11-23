7751ff712e318dc53b4f3763a65a2d2499a9d90c
Aditya Ganesh Shanbhag <aditya.shanbhag.dev@gmail.com>
2025-11-23 16:10:18 +0530

    [Added] - metadata, og img; feat: Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
@@ -8,24 +9,53 @@ import Background from "@/components/background";
 
 export const metadata: Metadata = {
   title: "Vibreon - Music Video Generator",
-  description: `Create aesthetic AI-powered music videos with audio effects, HD visuals, lyrics, and more. 
-                Upload an MP3 or YouTube link, choose an effect, pick a theme, and generate stunning videos instantly.`,
-  keywords: [
-    "Vibreon",
-    "AI music video generator",
-    "audio effects",
-    "FFmpeg",
-    "slowed reverb",
-    "nightcore",
-    "8D audio",
-    "cloudinary",
-    "pexels",
-    "ai subtitles",
-    "music visualizer",
-    "video editor",
-    "lofi",
-  ],
+
+  description: `Create aesthetic music videos with audio effects, HD visuals, lyrics, and more. Upload an MP3 
+    or YouTube link, choose an effect, pick a theme for the background, and generate stunning videos instantly.`,
+
+  keywords: ["Vibreon", "AI music video generator", "audio effects", "FFmpeg", "yt-dlp", "slowed reverb", "nightcore",
+    "8D audio", "cloudinary", "pexels", "ai subtitles", "music visualizer", "video editor", "lofi"],
+
+  authors: [{ name: "Vibreon", url: "https://vibreon.vercel.app" }],
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+
+  openGraph: {
+    url: "https://vibreon.vercel.app",
+    type: "website",
+    title: "Vibreon - Music Video Generator",
+    description: `Create aesthetic music videos with audio effects, HD visuals, lyrics, and more. Upload an MP3 
+      or YouTube link, choose an effect, pick a theme for the background, and generate stunning videos instantly.`,
+    images: [
+      {
+        url: "https://vibreon.vercel.app/og.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Vibreon - Music Video Generator",
+    description: `Create aesthetic music videos with audio effects, HD visuals, lyrics, and more. Upload an MP3 
+      or YouTube link, choose an effect, pick a theme for the background, and generate stunning videos instantly.`,
+    images: ["https://vibreon.vercel.app/og.png"],
+  },
 };
+
 
 export default function RootLayout({
   children,
@@ -47,6 +77,7 @@ export default function RootLayout({
             {children}
             <Footer />
             <Toaster />
+            <Analytics />
           </div>
         </ThemeProvider>
       </body>
